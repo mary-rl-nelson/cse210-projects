7d86ad092d2f358d46e5b78d795fcd2a2a3edef3
mary-rl-nelson <mary.rl.nelson@gmail.com>
2025-09-29 20:44:20 -0500

    Adding while loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string number = Console.ReadLine();
+        int magicNumber = int.Parse(number);
+
+        Console.Write("What is you guess? ");
+        string guess = Console.ReadLine();
+        int userGuess = int.Parse(guess);
+
+        if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+
+        while (magicNumber != userGuess)
+        {
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            userGuess = int.Parse(guess);
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        }
+
+        Console.WriteLine("You guessed it!!");
     }
 }

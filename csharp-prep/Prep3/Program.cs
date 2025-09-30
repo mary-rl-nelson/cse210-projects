29df8e5a02c99d69afeaefe264366e88cc43245c
mary-rl-nelson <mary.rl.nelson@gmail.com>
2025-09-29 21:09:47 -0500

    cleaned up and simplified code/got rid of unneeded repetition. Added random number generator. Added guess counter.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,33 +1,20 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        string number = Console.ReadLine();
-        int magicNumber = int.Parse(number);
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
 
-        Console.Write("What is you guess? ");
-        string guess = Console.ReadLine();
-        int userGuess = int.Parse(guess);
+        int guessNumber = 1;
+        Console.Write("What is your guess? ");
+        int userGuess = int.Parse(Console.ReadLine());
 
-        if (userGuess > magicNumber)
+        while (userGuess != magicNumber)
         {
-            Console.WriteLine("Lower");
-        }
-        else if (userGuess < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-
-
-        while (magicNumber != userGuess)
-        {
-            Console.Write("What is your guess? ");
-            guess = Console.ReadLine();
-            userGuess = int.Parse(guess);
-
             if (userGuess > magicNumber)
             {
                 Console.WriteLine("Lower");
@@ -37,8 +24,12 @@ class Program
                 Console.WriteLine("Higher");
             }
 
+            guessNumber++;
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
         }
 
-        Console.WriteLine("You guessed it!!");
+        Console.WriteLine($"You guessed it!! It took you {guessNumber} tries.");
     }
+
 }

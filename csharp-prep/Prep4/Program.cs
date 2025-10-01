5541a85152211885e7840e5225fb3c964a4a7447
mary-rl-nelson <mary.rl.nelson@gmail.com>
2025-09-30 21:09:21 -0500

    Completed part 3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Collections.Concurrent;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        int count = 0;
+        double sum = 0;
+        
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+        }
+        foreach (int num in numbers)
+        {
+            count++;
+            if (count == numbers.Count)
+            {
+                Console.WriteLine(num); // Last item — no comma
+            }
+            else
+            {
+                Console.WriteLine(num + ", ");
+            }
+        }
+
+        foreach (int value in numbers)
+        {
+            sum += value;
+        }
+        
+        int largestNumber = numbers[0];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > largestNumber)
+            {
+                largestNumber = numbers[i];
+            }
+        }
+
+        int countNumbers = numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / countNumbers;
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }

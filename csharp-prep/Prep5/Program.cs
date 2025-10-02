6aeb995ca2f7d7bfda1233877171764ed30f6b53
mary-rl-nelson <mary.rl.nelson@gmail.com>
2025-10-02 16:59:43 -0500

    Added necessary functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int year;
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromtUserNumber();
+        PromptUserBirthYear(out year);
+        int currentYear = GetCurrentYear();
+        int age = GetAge(currentYear, year);
+        int numberSquared = SquareNumber(number);
+        DisplayResult(name, numberSquared, age);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromtUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int GetCurrentYear()
+    {
+        DateTime currentTime = DateTime.Now;
+        int currentYear = currentTime.Year;
+        return currentYear;
+    }
+
+    static int GetAge(int currentYear, int year)
+    {
+        int age = currentYear - year;
+        return age;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+
+    static void DisplayResult(string name, int numberSquared, int age)
+    {
+        Console.WriteLine($"{name}, the square of your number is {numberSquared}.");
+        Console.WriteLine($"{name}, you will turn {age}.");
     }
 }

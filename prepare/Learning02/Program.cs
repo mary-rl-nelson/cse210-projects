ac371eb559e4669a93040513fd93591d9382529a
mary-rl-nelson <mary.rl.nelson@gmail.com>
2025-10-03 16:17:58 -0500

    Completed Resume Project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 1996;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Vet";
+        job2._company = "SpringVet";
+        job2._startYear = 2004;
+        job2._endYear = 2030;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mary Frazier";
+       
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

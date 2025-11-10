dbcbaeedc915b87b0d8a462bffbf17982f05c97f
mary-rl-nelson <mary.rl.nelson@gmail.com>
2025-11-10 13:08:05 -0600

    Updated/repracticed assignment

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,12 +1,12 @@
 public class Job
 {
-    public string _jobTitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
+    public string _company="";
+    public string _jobTitle = "";
+    public int _startYear = 0;
+    public int _endYear = 0;
 
     public void Display()
     {
-        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        Console.WriteLine($"{_jobTitle} for {_company}: {_startYear}-{_endYear}");
     }
-}
+    }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,23 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
+        job1._company = "Amazon";
         job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 1996;
-        job1._endYear = 2026;
+        job1._startYear = 1997;
+        job1._endYear = 2018;
+        //job1.Display();
 
         Job job2 = new Job();
-        job2._jobTitle = "Vet";
-        job2._company = "SpringVet";
-        job2._startYear = 2004;
-        job2._endYear = 2030;
+        job2._company = "Google";
+        job2._jobTitle = "Software Designer";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+        //job2.Display();
 
-        Resume myResume = new Resume();
-        myResume._name = "Mary Frazier";
-       
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+        Job job3 = new Job();
+        job3._company = "Microsoft";
+        job3._jobTitle = "Software Designer";
+        job3._startYear = 2018;
+        job3._endYear = 2024;
+        //job3.Display();
 
-        myResume.Display();
+        Job job4 = new Job();
+        job4._company = "Tootles";
+        job4._jobTitle = "Software Designer";
+        job4._startYear = 2018;
+        job4._endYear = 2024;
+        //job4.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Alice Jackson";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+        //Console.WriteLine(resume1._jobs[1]._jobTitle);
+
+        Resume resume2 = new Resume();
+        resume2._name = "Jack Butts";
+        resume2._jobs.Add(job3);
+        resume2._jobs.Add(job4);
+        resume2.Display();
+
     }
 }
